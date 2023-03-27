98a62dba644f66d2d3be9cf2efc8d6737047a046
Tony Tran <tonytran1645@gmail.com>
2023-03-27 19:57:43 -0400

    Implemented a createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,58 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
+const renderTweets = function(tweets) {
+
+};
+
+const createTweetElement = function(tweet) {
+  let $tweet = `
+    <article>
+      <header>
+        <div class="tweet-avatar-name">
+          <img src=${tweetData.user.avatars}>
+          <h3>${tweetData.user.name}</h3>
+        </div>
+        <h4>${tweetData.user.handle}</h4>
+      </header>
+      <div class="tweet-content">
+        ${escape(tweetData.content.text)} 
+      </div>
+      <footer>
+        <div>${moment(tweetData.created_at).fromNow()}</div>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
+  return $tweet; 
+};
+
+renderTweets(data);
