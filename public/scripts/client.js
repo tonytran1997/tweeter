322a3b0ecdbee9a2cde8facc2be81ee1cc758ceb
Tony Tran <tonytran1645@gmail.com>
2023-03-29 14:25:03 -0400

    Implemented loadTweets and timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 const renderTweets = function(tweets) {
   for (let obj of tweets) {
@@ -59,9 +36,19 @@ const createTweetElement = function(tweet) {
   return $tweet; 
 };
 
-renderTweets(data);
+
+const loadTweets = () => {
+  $.ajax ({
+    url:"/tweets",
+    method: 'GET'
+  })
+  .then((data) => {
+    renderTweets(data);
+  })
+};
 
 $(document).ready(function() {
+  loadTweets()
   $("#new-tweets").submit(function(event) {
     event.preventDefault();
     console.log("Tweeter!")
